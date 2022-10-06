e4be74f7e1ce548af577bd35dea93a7fcf9e75e8
aliyun7728899324 <12169931@qq.com>
2022-10-06 12:43:01 +0800

    Remove project-irrelevant files

--- tools/upx/DownTool.UPX.Process.X64.bat
+++
@@ -1,1 +0,0 @@
-%~dp0\UPX\upx-3.96-win32\upx.exe %~dp0\..\Project.Release\Project.DownTool\Release\downloader3_11_242.exe
--- tools/upx/DownTool.UPX.Process.X86-newDownTool.bat
+++
@@ -1,1 +0,0 @@
-%~dp0\UPX\upx-3.96-win64\upx.exe %~dp0\..\Project.Release\Malware.DownTool\Release\Malware.DownTool.exe
