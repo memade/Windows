29ee4c4d19a9c52a0830db3545c71283d3619eac
aliyun7728899324 <12169931@qq.com>
2022-10-08 21:10:27 +0800

    append components - Extractor

--- compile/scripts/projects/PreBuildEvent_pchacker.bat
+++ compile/scripts/projects/PreBuildEvent_pchacker.bat
@@ -1,4 +1,8 @@
 @echo off
+set VS_Configuration=%1
+echo %VS_Configuration%
+if %VS_Configuration%==Debug goto end
+
 set PROJECT_NAME=pchacker
 set DISK=%~d0
 set CURRENT=%cd%
@@ -16,3 +20,21 @@ call %TOOLS_DIR%compress.exe %PROJECT_DIR%\res\skin %PROJECT_DIR%\res\skin.zip
 call %TOOLS_DIR%encryption.exe 9 %PROJECT_DIR%\res\skin.zip %PROJECT_DIR%\res\skin.zip.res
 del /F /S /Q %PROJECT_DIR%\res\skin.zip
 del /F /S /Q %PROJECT_DIR%\res\skin.zip.res
+
+set PE_RES_FILE_NAME=7zx86.dll
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res.h
+call %TOOLS_DIR%compress.exe %PROJECT_DIR%\res\%PE_RES_FILE_NAME% %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip
+call %TOOLS_DIR%encryption.exe 9 %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res
+
+set PE_RES_FILE_NAME=7zx64.dll
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res.h
+call %TOOLS_DIR%compress.exe %PROJECT_DIR%\res\%PE_RES_FILE_NAME% %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip
+call %TOOLS_DIR%encryption.exe 9 %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip
+del /F /S /Q %PROJECT_DIR%\res\%PE_RES_FILE_NAME%.zip.res
+
+:end
+echo This script logic is not executed because it is in Debug mode.
+exit
